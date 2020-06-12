2f027fb185c1bb8dc75bf74513dbff8cae2b9601
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-12 15:03:12 -0400

    Update MoreBasic.ts

--- MoreBasic.ts
+++ MoreBasic.ts
@@ -1,7 +1,7 @@
 //% weight=100 color=#333333 icon="\uf100"
 namespace moreBasic{
 //% block
-export function when(y: boolean, x: ()=>void) {
+export function when(y: boolean, x: ()=>(void)) {
 
 
 
@@ -20,13 +20,7 @@ basic.forever(function () {
 }
 
 
-//% block
-export function whentest(c: ()=>void){
 
-console.log("hi")
-
-
-}
 
 
 
